54c96491ba151fd523eae9ebe67fa1a030ed5126
Gabriel Raul Andrade Mora <gabriel.raul.andrade.mora@gmail.com>
2026-03-29 18:52:12 -0500

    chore(api): initialize Cypress project with reporter

---
+++ API/cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "reports",
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
+  e2e: {
+    baseUrl: "https://petstore.swagger.io/v2",
+    specPattern: "cypress/e2e/**/*.cy.js",
+    supportFile: "cypress/support/e2e.js",
+    video: false,
+    setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
+      return config;
+    },
+  },
+});
